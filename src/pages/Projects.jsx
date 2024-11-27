a64e85a1f26317ff54911e53fff9d1c3151c050d
Keyurrr14 <keyurrathod9920@gmail.com>
2024-11-27 23:14:04 +0530

    ServicesPage and Component added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,22 +1,22 @@
 import HomeImage3 from "../assets/images/HomeImage3.webp";
 import HomeImage4 from "../assets/images/HomeImage4.webp";
-import LogoMarquee from "../components/LogoMarquee";
+// import LogoMarquee from "../components/LogoMarquee";
 import ProjectSection from "../components/ProjectSection";
 
 export default function Projects() {
   return (
     <div className="min-h-screen bg-beige">
-      <div className="flex flex-col items-center justify-center mt-10">
-        <h1 className="font-marbley text-center text-brown text-9xl">
+      <div className="flex flex-col items-center justify-center mt-4 sm:mt-6 md:mt-8 lg:mt-10 px-4">
+        <h1 className="font-marbley text-center text-brown text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl">
           Projects
         </h1>
-        <p className="w-96 font-manrope text-center text-brown">
+        <p className="w-full sm:w-[28rem] font-manrope text-center text-brown text-xs sm:text-base md:text-lg mt-2 sm:mt-4">
           Our interior design process for each project is completely unique. We
-          take into consideration the client’s style, personality and specific
+          take into consideration the client's style, personality and specific
           needs to ensure their vision is executed perfectly.
         </p>
       </div>
-      <div className="mt-5"></div>
+      <div className="mt-4 sm:mt-5"></div>
       {/* <LogoMarquee /> */}
       <ProjectSection
         type="Residential"
@@ -25,6 +25,7 @@ export default function Projects() {
         image2={HomeImage3}
         order={[1, 2, 3]}
       />
+      <hr className="w-full mx-4 border-brown my-8 sm:my-10 md:my-12" />
       <ProjectSection
         type="Residential"
         location="Oberoi Sky City"
@@ -32,6 +33,7 @@ export default function Projects() {
         image2={HomeImage3}
         order={[3, 2, 1]}
       />
+      
       <ProjectSection
         type="Residential"
         location="Oberoi Sky City"
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,65 @@
+import ServiceSection from "../components/ServiceSection";
+
 export default function Services() {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-3xl font-manrope font-semibold text-brown">Our Services</h1>
+  return (
+    <div className="min-h-screen bg-beige">
+      <div className="flex flex-col items-center justify-center mt-4 sm:mt-6 md:mt-8 lg:mt-10 px-4">
+        <h1 className="font-marbley text-center text-brown text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl max-w-3xl">
+          Comprehensive Architecture and Interior Design Services in Mumbai
+        </h1>
+        <p className="w-full sm:w-[28rem] font-manrope text-center text-brown text-xs sm:text-base md:text-lg mt-2 sm:mt-4">
+          Explore our range of professional services designed to cater to all
+          your architectural and interior design needs.
+        </p>
       </div>
-    );
-  }
+
+      <ServiceSection
+        title="Architecture Design"
+        number={1}
+        description="Reimagine your spaces with innovative architectural solutions that inspire and delight."
+        services={[
+          "Custom Residential & Commercial Building Designs",
+          "Structural Planning and 3D Modeling",
+          "Sustainable Architecture Solutions",
+          "Local Building Code Compliance and Permits",
+        ]}
+      />
+
+      <ServiceSection
+        title="Interior Design"
+        number={2}
+        description="Infuse personality into your spaces with curated interior design solutions."
+        services={[
+          "Space Planning and Optimized Layouts",
+          "Bespoke Furniture Design ans Styling",
+          "Mood Boards, Material Selections & Colour Coordination",
+          "Lighting Design to enhance ambience",
+        ]}
+      />
+
+      <ServiceSection
+        title="Design & Build Services"
+        number={3}
+        description="From concept to completion, we handle every aspect of your project."
+        services={[
+          "Turnkey Solutions for Residential, Commercial & Hospitality Spaces",
+          "Budget Planning and Cost Estimation",
+          "Construction Supervision and Vendor Coordination",
+          "Quality Assurance and Final Handover",
+        ]}
+      />
+
+      <ServiceSection
+        title="Project Management"
+        number={4}
+        description="Delivering projects efficiently, effectively, and stress-free"
+        services={[
+          "Detailed Project Scheduling and Monitoring",
+          "Resource Allocation and Cost Control",
+          "On-Site Quality Checks and Vendor Management",
+          "Timely Project Deliverables and Handovers",
+        ]}
+      />
+    </div>
+  );
+}
