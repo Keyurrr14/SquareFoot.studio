ea76b802b18e88abe27c486cd5988959cd4636b8
Keyurrr14 <keyurrathod9920@gmail.com>
2024-12-21 13:38:08 +0530

    AnimatedCursor addd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+import AnimatedCursor from "react-animated-cursor";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { SmoothScrollProvider } from "./context/SmoothScrollContext";
 import RootLayout from "./layout/RootLayout";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact"
 export default function App() {
   return (
     <Router>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "black",
+        }}
+        outerStyle={{
+          backgroundColor: "black",
+        }}
+      />
       <SmoothScrollProvider>
         <Routes>
           <Route element={<RootLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />}/>
+            <Route path="/contact" element={<Contact />} />
           </Route>
         </Routes>
       </SmoothScrollProvider>
